e1dd9c06c8bc2851d4909a11ae156a59a82d63be
PritamDas <pritam.das.santu@gmail.com>
2024-07-10 22:53:43 +0530

    deleted main branch

--- backend/src/message.ts
+++
@@ -1,4 +0,0 @@
-export const INIT_MESSAGE = "init_game";
-export const MOVE_MESSAGE = "move";
-export const GAME_OVER = "gameover";
-export const WS_URL = "ws://localhost:8080";
--- frontend/src/providers.tsx
+++
@@ -1,5 +0,0 @@
-import { BrowserRouter } from "react-router-dom";
-
-export const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <BrowserRouter>{children}</BrowserRouter>;
-};
